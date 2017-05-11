944121275a4604ded5cbba5fdb0dfe985605da37
JACK <s4851798@gmail.com>
2017-05-12 03:24:11 +0800

    Handle new test data

--- src/main/java/Container/MEPAMembershipMap.java
+++ src/main/java/Container/MEPAMembershipMap.java
@@ -78,11 +78,11 @@ public class MEPAMembershipMap {
         return getAllInstanceByAttr(instances.getAttribute(curAttrInd));
     }
 
-    public ArrayList<String> getAttributeValue(Attribute curAttr){
+    private ArrayList<String> getAttributeValue(Attribute curAttr){
         return attributeValueMap.get(curAttr);
     }
 
-    public ArrayList<String> getAttributeValue(int curAttrInd){
+    private ArrayList<String> getAttributeValue(int curAttrInd){
         return getAttributeValue(instances.getAttribute(curAttrInd));
     }
 
